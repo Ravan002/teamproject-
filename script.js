ffb49b93133e876ec11f1bd0fd6f699afba047f6
Ravan002 <95362051+Ravan002@users.noreply.github.com>
2021-12-05 21:22:45 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+function myFunction() {
+    document.getElementById("p1").innerHTML = "Copyright: ARAM :)";
+}
+function myFunction2() {
+    document.getElementById("p1").innerText = "LADIES & GENTLEMEN\nWELCOME TO ARAM TEAM WEB SITE";
+}
